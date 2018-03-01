6f5aba6ddff802b5a29caba8ac3541bb622f12fc
Philip Korsholm <phko@itu.dk>
2018-03-01 19:53:24 +0100

    updated main.java

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,11 +1,17 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public int GlobalTime;
+    public static ArrayList<Trip> trips;
+    public static ArrayList<Car> cars;
     public static void main(String[] args) throws FileNotFoundException {
+
+        trips = new ArrayList<Trip>();
+        cars = new ArrayList<Car>();
 
         if (args.length <= 0) System.exit(0);
         Scanner scanner = new Scanner(new FileInputStream(args[0]));
@@ -14,6 +20,24 @@ public class Main {
         int numCols = scanner.nextInt();
         int vehicles = scanner.nextInt();
         int rides = scanner.nextInt();
+        int bonus = scanner.nextInt();
         int time = scanner.nextInt();
+        scanner.nextLine();
+        while(scanner.hasNextLine()){
+            int startx = scanner.nextInt();
+            int starty = scanner.nextInt();
+            int endx = scanner.nextInt();
+            int endy = scanner.nextInt();
+            int earliestStart = scanner.nextInt();
+            int latestEnd = scanner.nextInt();
+            Tuple s = new Tuple(startx,starty);
+            Tuple e = new Tuple(startx,starty);
+            trips.add(new Trip(s, e, earliestStart, latestEnd));
+        }
+
+        for(int i = 0; i < vehicles; i++){
+            cars.add(new Car());
+        }
+
     }
 }
