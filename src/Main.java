b09f594056e6bfa6110668760f0af76afc00572b
Marek Kisiel <maki@itu.dk>
2018-03-01 20:08:01 +0100

    Revert "Added package statement"
    This reverts commit d1ab5564f33fb1a4855906b6b7c543ad945a2979.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import java.io.BufferedReader;
+import src.Tuple;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
--- src/Tuple.java
+++ src/Tuple.java
@@ -1,3 +1,5 @@
+package src;
+
 public class Tuple {
     int x;
     int y;
